ac7e31cb389cfef9066c1ce12b559078cbfde982
Jaiditya Batra <vampconnoisseur@gmail.com>
2024-12-20 13:57:20 +0530

    add boilerplate cpp

---
+++ boilerplate.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define vvi vector<vector<int>>
+
+#define minpq priority_queue<int, vector<int>, greater<int>>
+#define maxpq priority_queue<int>
+
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define fore(e, a) for (auto &e : a)
+
+#define all(v) v.begin(), v.end()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+void solve() {}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
