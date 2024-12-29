01739f6ae4daee3392d45cb6f8a3919b204e9fd0
Jaiditya Batra <vampconnoisseur@gmail.com>
2024-12-30 00:34:00 +0530

    TLE - 1200(10+11+12)

---
+++ tle_elim/1200/scuza.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define INF 1000000000
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define vpii vector<pair<int, int>>
+#define si set<int>
+#define sll set<long long>
+#define spii set<pair<int, int>>
+#define minpq priority_queue<int, vector<int>, greater<int>>
+#define maxpq priority_queue<int>
+#define SORT(a) sort(a.begin(), a.end())
+#define REVSORT(a, T) sort(a.begin(), a.end(), greater<T>())
+#define rev(a) reverse(a.begin(), a.end())
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define fore(e, a) for (auto &e : a)
+#define all(v) v.begin(), v.end()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,lzcnt,fma,tune=native")
+
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+
+    vi maxArr(n);
+    vll pref(n);
+
+    int maxi = INT_MIN;
+    rep(i, 0, n)
+    {
+        cin >> maxArr[i];
+        pref[i] = (ll)maxArr[i] + (i > 0 ? pref[i - 1] : 0);
+
+        if (maxArr[i] > maxi)
+            maxi = maxArr[i];
+        else
+            maxArr[i] = maxi;
+    }
+
+    rep(i, 0, q)
+    {
+        int e;
+        cin >> e;
+        int idx = upper_bound(all(maxArr), e) - maxArr.begin() - 1;
+        cout << (idx > -1 ? pref[idx] : 0) << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
