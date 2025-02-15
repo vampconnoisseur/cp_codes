ef97b300f43f8402a955fcbfb50ea9199ce80338
Jaiditya Batra <vampconnoisseur@gmail.com>
2025-02-15 17:31:03 +0530

    striver cp - 1300

---
+++ striver_cp/1300/segment_occurences.cpp
@@ -0,0 +1,72 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define INF 1000000000
+#define ll long long
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<long long, long long>>
+#define minpq priority_queue<int, vector<int>, greater<int>>
+#define maxpq priority_queue<int>
+#define SORT(a) sort(a.begin(), a.end())
+#define REVSORT(a, T) sort(a.begin(), a.end(), greater<T>())
+#define rev(a) reverse(a.begin(), a.end())
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define fore(e, a) for (auto &e : a)
+#define all(v) v.begin(), v.end()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+int gcd(int a, int b) { if (b == 0) { return a; } return gcd(b, a % b); }
+void sieve(vector<bool> &primes, int N) { primes[0] = primes[1] = true; for (int i = 2; i * i <= N; i++) { if (!primes[i]) { for (int j = i * i; j <= N; j += i) { primes[j] = true; } } } }
+ll binpow(ll a, ll b) { ll res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+ll binpowMod(ll a, ll b) { a %= MOD; int res = 1; while (b > 0) { if (b & 1) res = res * a % MOD; a = a * a % MOD; b >>= 1; } return res; }
+void addPrimeDivs(int x, map<int, int> &divs) { int i = 2; while (i * i <= x) { while (x % i == 0) { divs[i]++; x /= i; } i++; } if (x > 1) divs[x]++; }
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,lzcnt,fma,tune=native")
+// clang-format on
+
+void solve()
+{
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    string s, t;
+    cin >> s >> t;
+
+    vi occ;
+    rep(i, 0, n - m + 1) if (s.substr(i, m) == t) occ.pb(i + 1);
+
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        int left = lower_bound(all(occ), l) - occ.begin();
+        int right = upper_bound(all(occ), r - m + 1) - occ.begin();
+
+        cout << max(0, right - left) << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
